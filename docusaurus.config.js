e6df494ce4fc0119177e34ecdda3eb17276297ec
Hirusha Adikari <hirushaadi@gmail.com>
2025-12-21 18:39:04 +1100

    minimally configured config with sidebars working

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,13 +4,14 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from 'prism-react-renderer';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  // TODO: Update the base config
+  title: 'ACUCyS',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -52,21 +53,25 @@ const config = {
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
-        },
+        // ----------
+        // BLOG: Uncomment to enable blogs to navbar.
+        // -----
+        // blog: {
+        //   showReadingTime: true,
+        //   feedOptions: {
+        //     type: ['rss', 'atom'],
+        //     xslt: true,
+        //   },
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   editUrl:
+        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        //   // Useful options to enforce blogging best practices
+        //   onInlineTags: 'warn',
+        //   onInlineAuthors: 'warn',
+        //   onUntruncatedBlogPosts: 'warn',
+        // },
+        // ----------
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -83,26 +88,39 @@ const config = {
         respectPrefersColorScheme: true,
       },
       navbar: {
-        title: 'My Site',
+        title: 'ACUCyS',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'ACUCyS Logo',
           src: 'img/logo.svg',
         },
         items: [
           {
             type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
+            sidebarId: 'ctfSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Capture The Flag',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
+            type: 'docSidebar',
+            sidebarId: 'hackathonSidebar',
+            position: 'left',
+            label: 'Hackathon',
           },
+          {
+            type: 'docSidebar',
+            sidebarId: 'conferenceSidebar',
+            position: 'left',
+            label: 'Conference',
+          },
+          // ----------
+          // BLOG: Uncomment to add blogs to navbar.
+          // -----
+          // { to: '/blog', label: 'Blog', position: 'left' },
+          // ----------
         ],
       },
+
+      // TODO: Update footer
       footer: {
         style: 'dark',
         links: [
